2b5ed7ecf27c51340d8a891b6dfa6eab38369ca4
Tyler Steck <steckdev@gmail.com>
2013-02-26 20:07:36 -0700

    Adding PomodoroPad

---
+++ PomodoroPad/PomodoroPad/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  PomodoroPad
+//
+//  Created by SteckDEV on 2/24/13.
+//  Copyright (c) 2013 com.steckdev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ PomodoroPad/PomodoroPad/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PomodoroPad
+//
+//  Created by SteckDEV on 2/24/13.
+//  Copyright (c) 2013 com.steckdev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
