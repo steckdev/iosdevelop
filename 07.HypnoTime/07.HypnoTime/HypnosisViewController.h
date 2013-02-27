f430a577b566fca2b3de656a0f67202d500c7d77
Tyler Steck <steckdev@gmail.com>
2013-02-26 20:24:49 -0700

    07.HypnoTime - Added HypnosisViewController to AppDelegate

---
+++ 07.HypnoTime/07.HypnoTime/HypnosisViewController.h
@@ -0,0 +1,13 @@
+//
+//  HypnosisViewController.h
+//  07.HypnoTime
+//
+//  Created by SteckDEV on 2/26/13.
+//  Copyright (c) 2013 com.steckdev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HypnosisViewController : NSObject
+
+@end
---
+++ 07.HypnoTime/07.HypnoTime/HypnosisViewController.m
@@ -0,0 +1,13 @@
+//
+//  HypnosisViewController.m
+//  07.HypnoTime
+//
+//  Created by SteckDEV on 2/26/13.
+//  Copyright (c) 2013 com.steckdev. All rights reserved.
+//
+
+#import "HypnosisViewController.h"
+
+@implementation HypnosisViewController
+
+@end
