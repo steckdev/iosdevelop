e26b7bf31386ee34d531a81c45e58272193bbee5
Tyler Steck <steckdev@gmail.com>
2013-02-26 20:12:39 -0700

    07.HypnoTime - Initial Commit

---
+++ 07.HypnoTime/07.HypnoTime/HypnoAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HypnoAppDelegate.h
+//  07.HypnoTime
+//
+//  Created by SteckDEV on 2/26/13.
+//  Copyright (c) 2013 com.steckdev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnoAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
