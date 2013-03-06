dfef5e9d43aa03445dfb6fe9c85cf25a83f2d082
Tyler Steck <steckdev@gmail.com>
2013-03-06 12:17:21 -0700

    UIDevice Notification

--- 08.HeavyRotation/08.HeavyRotation/RotationAppDelegate.m
+++ 08.HeavyRotation/08.HeavyRotation/RotationAppDelegate.m
@@ -14,9 +14,27 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+    
+    UIDevice *device = [UIDevice currentDevice];
+    // Tell it to start monitoring the accelerometer for orientation
+    [device beginGeneratingDeviceOrientationNotifications];
+    // Get the notification center for the app
+    NSNotificationCenter *nc = [NSNotificationCenter defaultCenter];
+    // Add yourself as an observer
+    [nc addObserver:self
+           selector:@selector(orientationChanged:)
+               name:UIDeviceOrientationDidChangeNotification
+             object:device];
+    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
+}
+
+- (void)orientationChanged:(NSNotification *)note
+{
+    // Log the constant that represents the current orientation
+    NSLog(@"orientationChanged: %d", [[note object] orientation]);
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application
