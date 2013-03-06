fcfe9be23c1fd8b7e28e181807719bca621f71bc
Tyler Steck <steckdev@gmail.com>
2013-03-06 11:26:13 -0700

    Adding 08.HeavyRotation

---
+++ 08.HeavyRotation/08.HeavyRotation/RotationAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RotationAppDelegate.h
+//  08.HeavyRotation
+//
+//  Created by SteckDEV on 3/6/13.
+//  Copyright (c) 2013 com.steckdev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RotationAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 08.HeavyRotation/08.HeavyRotation/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  08.HeavyRotation
+//
+//  Created by SteckDEV on 3/6/13.
+//  Copyright (c) 2013 com.steckdev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RotationAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RotationAppDelegate class]));
+    }
+}
